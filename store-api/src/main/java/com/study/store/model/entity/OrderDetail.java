36be5089c53ff98b6c4601885d9ae62095eaee04
김형준 <gudwns026@naver.com>
2021-01-12 21:54:58 +0900

    update OrderDetail variable

--- store-api/src/main/java/com/study/store/model/entity/OrderDetail.java
+++ store-api/src/main/java/com/study/store/model/entity/OrderDetail.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
@@ -17,13 +18,12 @@ public class OrderDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String status;
+    private LocalDateTime arrivalDate;
+    private Integer quantity;
+    private BigDecimal totalPrice;
     private LocalDateTime createdAt;
-
-    @ManyToOne
-    private User user;
-
-    private Long orderGroupId;
-
-    @ManyToOne
-    private Item item;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
 }
