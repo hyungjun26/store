1c8ce8384fd397d8a800045a1c72e5c0c69493d0
김형준 <gudwns026@naver.com>
2021-01-12 22:06:05 +0900

    Update AdminUser Entity

--- store-api/src/main/java/com/study/store/model/entity/AdminUser.java
+++ store-api/src/main/java/com/study/store/model/entity/AdminUser.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -14,6 +17,8 @@ import java.time.LocalDateTime;
 @Entity
 public class AdminUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String account;
     private String password;
