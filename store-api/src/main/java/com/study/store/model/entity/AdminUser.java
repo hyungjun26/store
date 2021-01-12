36350da6cb8056dc6b4a563fa0e84e11378adfa9
김형준 <gudwns026@naver.com>
2021-01-12 22:04:56 +0900

    Add AdminUser Entity

---
+++ store-api/src/main/java/com/study/store/model/entity/AdminUser.java
@@ -0,0 +1,31 @@
+package com.study.store.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AdminUser {
+
+    private Long id;
+    private String account;
+    private String password;
+    private String status;
+    private String role;
+    private LocalDateTime lastLoginAt;
+    private LocalDateTime passwordUpdatedAt;
+    private int loginFailCount;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
