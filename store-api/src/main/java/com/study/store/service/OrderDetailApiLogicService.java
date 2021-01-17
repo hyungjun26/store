d45bf10924fd1de70642951230e89e94c79762d4
김형준 <gudwns026@naver.com>
2021-01-17 22:29:53 +0900

    Add OrderDetailApi

---
+++ store-api/src/main/java/com/study/store/service/OrderDetailApiLogicService.java
@@ -0,0 +1,97 @@
+package com.study.store.service;
+
+import com.study.store.interfaces.CrudInterface;
+import com.study.store.model.entity.OrderDetail;
+import com.study.store.model.network.Header;
+import com.study.store.model.network.request.OrderDetailApiRequest;
+import com.study.store.model.network.response.OrderDetailApiResponse;
+import com.study.store.repository.ItemRepository;
+import com.study.store.repository.OrderDetailRepository;
+import com.study.store.repository.OrderGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OrderDetailApiLogicService implements CrudInterface<OrderDetailApiRequest, OrderDetailApiResponse> {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Override
+    public Header<OrderDetailApiResponse> create(Header<OrderDetailApiRequest> request) {
+        OrderDetailApiRequest body = request.getData();
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .status(body.getStatus())
+                .arrivalDate(body.getArrivalDate())
+                .quantity(body.getQuantity())
+                .totalPrice(body.getTotalPrice())
+                .orderGroup(orderGroupRepository.getOne(body.getOrderGroupId()))
+                .item(itemRepository.getOne(body.getItemId()))
+                .build();
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+
+        return response(newOrderDetail);
+    }
+
+    @Override
+    public Header<OrderDetailApiResponse> read(Long id) {
+        return orderDetailRepository.findById(id)
+                .map(orderDetail -> response(orderDetail))
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<OrderDetailApiResponse> update(Header<OrderDetailApiRequest> request) {
+        OrderDetailApiRequest body = request.getData();
+
+        Optional<OrderDetail> optional = orderDetailRepository.findById(body.getId());
+
+        return optional
+                .map(orderDetail -> {
+                    orderDetail
+                            .setStatus(body.getStatus())
+                            .setArrivalDate(body.getArrivalDate())
+                            .setQuantity(body.getQuantity())
+                            .setTotalPrice(body.getTotalPrice())
+                            .setOrderGroup(orderGroupRepository.getOne(body.getOrderGroupId()))
+                            .setItem(itemRepository.getOne(body.getItemId()));
+                    return orderDetail;
+                })
+                .map(orderDetail -> orderDetailRepository.save(orderDetail))
+                .map(orderDetail -> response(orderDetail))
+                .orElseGet(() -> Header.ERROR("데이터없음"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return orderDetailRepository.findById(id)
+                .map(orderDetail -> {
+                    orderDetailRepository.delete(orderDetail);
+                    return Header.OK();
+                })
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    private Header<OrderDetailApiResponse> response(OrderDetail orderDetail){
+        OrderDetailApiResponse body = OrderDetailApiResponse.builder()
+                .id(orderDetail.getId())
+                .status(orderDetail.getStatus())
+                .arrivalDate(orderDetail.getArrivalDate())
+                .quantity(orderDetail.getQuantity())
+                .totalPrice(orderDetail.getTotalPrice())
+                .orderGroupId(orderDetail.getOrderGroup().getId())
+                .itemId(orderDetail.getItem().getId())
+                .build();
+        return Header.OK(body);
+    }
+}
