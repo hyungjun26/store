3112a259971c813b573f0bb33022a559a1b1efc5
김형준 <gudwns026@naver.com>
2021-02-24 01:26:28 +0900

    Add UserJwtResponse

---
+++ store-api/src/main/java/com/study/store/model/network/response/UserJwtResponse.java
@@ -0,0 +1,14 @@
+package com.study.store.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserJwtResponse {
+    private String token;
+}
